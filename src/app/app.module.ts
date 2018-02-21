9bc5a9de1de0991a72d90d662d8d44524af03181
mahim3 <mahespappini7@gmail.com>
2018-02-21 14:08:39 +0530

    Auth0 with jwt token generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import 'rxjs/Rx';
 import { AppComponent } from './app.component';
 import { FormsModule , ReactiveFormsModule }   from '@angular/forms';
 import { RoutingModule, routingComponent } from './routing/routing.module';
--- src/app/services/auth0.service.ts
+++ src/app/services/auth0.service.ts
@@ -12,29 +12,33 @@ export class Auth0Service {
   constructor() {
     // Add callback for lock `authenticated` event
     this.lock.on("authenticated", (authResult:any) => {
-      this.lock.getProfile(authResult.idToken, function(error:any, profile:any){
+      this.lock.getProfile(authResult.accessToken, function(error:any, profile:any){
           if(error){
               throw new Error(error);
           }
-            localStorage.setItem('id_token', authResult.idToken);
+            localStorage.setItem('id_token', authResult.accessToken);
             localStorage.setItem('profile', JSON.stringify(profile)); 
       });
     });
+
   }
 
   public login() {
     // Call the show method to display the widget.
+    console.log("from login");    
     this.lock.show();
   };
 
   public authenticated() {
     // Check if there's an unexpired JWT
     // This searches for an item in localStorage with key == 'id_token'
+    console.log("authenticated");    
     return tokenNotExpired();
   };
 
   public logout() {
     // Remove token from localStorage
+    console.log("from logout");
     localStorage.removeItem('id_token');
     localStorage.removeItem('profile');
   };
